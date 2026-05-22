0fc8371ceb6137de155015fd173a7ecf52930049
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-23 03:01:47 +0600

    Project setup ,database Created

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express"
+const app = express()
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
+
+app.get('/', (req, res) => {
+  res.json({
+    Project : "Bug DevPulse",
+  })
+})
+
+
+export default app;
