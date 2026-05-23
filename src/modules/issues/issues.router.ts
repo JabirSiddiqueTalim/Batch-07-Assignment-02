de30a3981ff776633100c13814ce15528fbf3070
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-23 18:29:06 +0600

    Remove node_modules and .env from repository

--- src/modules/issues/issues.router.ts
+++ src/modules/issues/issues.router.ts
@@ -8,8 +8,7 @@ const router = Router()
 router.post('/',auth(UseRole.contributor, UseRole.maintainer),issueController.createIssue);
 router.get('/', issueController.getAllIssues);
 router.get("/:id", issueController.getSingleIssue);
-
-
+router.patch('/:id', )
 
 
 
